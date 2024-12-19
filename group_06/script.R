a10c136d4c3550f90ff45d09c123ecefa1eefc5f
GriffinH-Commits <gholmes@stevens.edu>
2024-12-19 08:28:37 -0500

    Untracked raw_data

--- group_06/script.R
+++ group_06/script.R
@@ -5,9 +5,13 @@ library(readr)
 raw_data <- "raw_data"
 dataset_url <- "https://data.iowa.gov/api/views/qd3t-kfqg/rows.csv?accessType=DOWNLOAD"
 
-
 # Define file path for dataset
 dataset_path <- file.path(raw_data, "Iowa_Economic_Indicators.csv")
+
+#Create raw_data folder if not already created
+if (!dir.exists(raw_data)) {
+  dir.create(raw_data)
+}
 
 # Check if dataset downloaded, if not download
 if (!file.exists(dataset_path)) {
@@ -16,9 +20,6 @@ if (!file.exists(dataset_path)) {
 
 # Read the CSV file
 Iowa_Economic_Indicators <- read_csv(dataset_path)
-
-# Make sure data is only object in environment
-rm(list = setdiff(ls(), "Iowa_Economic_Indicators"))
 
 # Print first 5 rows of dataset to make sure script is working
 print(head(Iowa_Economic_Indicators, 5))
